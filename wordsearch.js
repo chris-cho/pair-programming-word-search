9bc40ad7d0b01a10978c1df3ba195fa33b1cfd45
Chris Cho <hyunsangc@gmail.com>
2021-08-28 21:44:57 -0400

    add pair programming assignment

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,14 @@
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
+    //console.log(horizontalJoin);
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+    const verticalJoin = letters[0].map((_, index) => letters.map(row => row[index]));
+    for (l of verticalJoin) {
+        if (l.join('').includes(word)) return true
+    }
+    return false;
 }
 
-module.exports = wordSearch
+module.exports = wordSearch;
